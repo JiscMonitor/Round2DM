b818ff1dd6f3638b79ca087de9f4590594967ed7
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-09-20 12:12:39 +0100

    Create user roles in bootstrap

--- lorix/grails-app/conf/BootStrap.groovy
+++ lorix/grails-app/conf/BootStrap.groovy
@@ -1,11 +1,15 @@
 import grails.plugin.springsecurity.SpringSecurityUtils
 import grails.plugin.springsecurity.SecurityFilterPosition
 
+import uk.ac.jisc.lorix.*
 
 class BootStrap {
 
   def init = { servletContext ->
     SpringSecurityUtils.clientRegisterFilter('shibAuthFilter', SecurityFilterPosition.PRE_AUTH_FILTER)
+
+    def userRole = AuthCommonRole.findByAuthority('ROLE_USER') ?: new AuthCommonRole(authority: 'ROLE_USER', roleType:'global').save(failOnError: true)
+    def userAdmin = AuthCommonRole.findByAuthority('ROLE_ADMIN') ?: new AuthCommonRole(authority: 'ROLE_ADMIN', roleType:'global').save(failOnError: true)
   }
 
 

75337efd08928ae72fc2386546768d28872d4ba6
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-09-20 11:41:15 +0100

    Added shib filters

--- lorix/grails-app/conf/spring/resources.groovy
+++ lorix/grails-app/conf/spring/resources.groovy
@@ -1,3 +1,25 @@
 // Place your Spring DSL code here
 beans = {
+
+  userDetailsService(org.codehaus.groovy.grails.plugins.springsecurity.GormUserDetailsService) {
+    grailsApplication = ref('grailsApplication')
+  }
+
+  userDetailsByNameServiceWrapper(org.springframework.security.core.userdetails.UserDetailsByNameServiceWrapper) {
+    userDetailsService = ref('userDetailsService')
+  }
+
+  preAuthenticatedAuthenticationProvider(org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider) {
+    preAuthenticatedUserDetailsService = ref('userDetailsByNameServiceWrapper')
+  }
+
+  securityContextPersistenceFilter(org.springframework.security.web.context.SecurityContextPersistenceFilter){
+  }
+
+  shibAuthFilter(com.k_int.shib.ShibAuthFilter){
+    grailsApplication = ref('grailsApplication')
+    authenticationManager = ref('authenticationManager')
+  }
+
 }
+
---
+++ lorix/src/groovy/com/k_int/shib/ShibAuthFilter.groovy
@@ -0,0 +1,76 @@
+package com.k_int.shib
+
+import uk.ac.jisc.lorix.*
+
+public class ShibAuthFilter extends org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter {
+
+  private java.util.HashMap map = null;
+  def grailsApplication
+
+  def setEdiAuthTokenMap(java.util.HashMap map) {
+    this.map = map;
+  }
+
+  def getPreAuthenticatedPrincipal(javax.servlet.http.HttpServletRequest request) {
+
+    // log.debug("EdiauthFilter::getPreAuthenticatedPrincipal ${request}");
+
+    def result
+
+    if ( grailsApplication?.config?.authmethod=='shib' ) {
+      if ( request.getRemoteUser() != null ) {
+        // log.debug("In shibboleth authentication mode. If we're here - the user is pre-authenticated. Extract username and make sure there is a user record");
+        // User ID should be in request.getAttribute('persistent-id');
+        // log.debug("Remote User(fn):: ${request.getRemoteUser()}");
+        // log.debug("Remote User:: ${request.getAttribute('REMOTE_USER')}");
+        // log.debug("Persistent Id:: ${request.getAttribute('persistent-id')}");
+  
+
+        User.withTransaction { status ->
+          def existing_user = AuthCommonUser.findByUsername(request.getRemoteUser())
+          if ( existing_user ) {
+            // log.debug("User found, all is well");
+          }
+          else {
+            existing_user = new AuthCommonUser(
+                                     username:request.getRemoteUser(),
+                                     password:'**',
+                                     enabled:true,
+                                     accountExpired:false,
+                                     accountLocked:false,
+                                     passwordExpired:false,
+                                     email:request.getAttribute('email'))
+
+            if ( existing_user.save(flush:true) ) {
+              log.debug("Created user, allocating user role");
+              def userRole = AuthCommonRole.findByAuthority('ROLE_USER')
+  
+              if ( userRole ) {
+                log.debug("looked up user role: ${userRole}");
+                def new_role_allocation = new AuthCommonUserAuthCommonRole(authCommonUser:existing_user,authCommonRole:userRole);
+  
+                if ( new_role_allocation.save(flush:true) ) {
+                  log.debug("New role created...");
+                }
+                else {
+                    new_role_allocation.errors.each { e ->
+                    log.error(e);
+                  }
+                }
+              }
+              else {
+                log.error("Unable to look up ROLE_USER");
+              }
+            }
+          }
+        }
+
+        result = request.getRemoteUser()
+      }
+    }
+  }
+
+  def getPreAuthenticatedCredentials(javax.servlet.http.HttpServletRequest request) {
+    return "";
+  }
+}

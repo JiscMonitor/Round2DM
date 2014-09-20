e9d0f842d053882e53657cc08d69e77f62ecdd16
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-09-20 11:44:03 +0100

    Configure shib auth filter in bootstrap

--- lorix/grails-app/conf/BootStrap.groovy
+++ lorix/grails-app/conf/BootStrap.groovy
@@ -1,7 +1,13 @@
+import org.codehaus.groovy.grails.plugins.springsecurity.SecurityFilterPosition
+import org.codehaus.groovy.grails.plugins.springsecurity.SpringSecurityUtils
+
 class BootStrap {
 
-    def init = { servletContext ->
-    }
-    def destroy = {
-    }
+  def init = { servletContext ->
+    SpringSecurityUtils.clientRegisterFilter('shibAuthFilter', SecurityFilterPosition.PRE_AUTH_FILTER)
+  }
+
+
+  def destroy = {
+  }
 }

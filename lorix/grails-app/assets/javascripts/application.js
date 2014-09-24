2fa77731f21647b648ef573a604d6f708c04dadc
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-09-24 17:02:17 +0100

    Tring to figure out what causes JQuery problems

--- lorix/grails-app/assets/javascripts/application.js
+++ lorix/grails-app/assets/javascripts/application.js
@@ -5,10 +5,10 @@
 // You're free to add application-wide JavaScript to this file, but it's generally better 
 // to create separate JavaScript files as needed.
 //
-//= require bootstrap
 //= require jquery
+//= require bootstrap
 //= require sb-admin-2
-//= require_tree .
+//= require_tree
 //= require_self
 
 if (typeof jQuery !== 'undefined') {

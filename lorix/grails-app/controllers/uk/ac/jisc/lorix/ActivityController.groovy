1c9d6aabc12f13693b94ee06868db662865b1aed
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-10-08 15:25:39 +0100

    Step forward with ag/dynamics

---
+++ lorix/grails-app/controllers/uk/ac/jisc/lorix/ActivityController.groovy
@@ -0,0 +1,51 @@
+package uk.ac.jisc.lorix
+
+import grails.converters.*
+import grails.plugin.springsecurity.annotation.Secured
+import org.codehaus.groovy.grails.commons.GrailsClassUtils
+import grails.plugin.springsecurity.SpringSecurityUtils
+
+class ActivityController {
+
+  def springSecurityService
+
+  @Secured(['ROLE_USER', 'IS_AUTHENTICATED_FULLY'])
+  def edit() { 
+    // Step 1:: iterate through tree processing __oid entries
+    def oid_params = request.getParameterNames().findAll{it.endsWith('__oid')}
+    oid_params.sort()
+
+    def result = [:]
+
+    process(result, oid_params)
+
+    log.debug("result.root = ${result.root}");
+
+    redirect(controller:'home', action:'index');
+  }
+
+  // Return the context
+  def process(result, oidattrlist) {
+
+    result.root = ''
+
+    oidattrlist.each { oidattr ->
+      log.debug("Processing oidattr :: ${oidattr}");
+      def oid_parts = params[oidattr].split(':');
+      def access_path = oidattr.replace('.__oid','') // trim .__oid
+      if ( oid_parts[1]=='NEW' ) {
+        log.debug("Request to create a new instance of class ${oid_parts[0]}")
+        def domain_class = grailsApplication.getArtefact('Domain',oid_parts[0])
+        def rslt = domain_class.getClazz().newInstance();
+        log.debug("evaluate x.${access_path}=y");
+        groovy.util.Eval.xy(result,rslt,"x.${access_path}=y");
+      }
+      else {
+        log.debug("lookup existing oid...");
+      }
+      log.debug("set ${access_path} to ${params[oidattr]}");
+      
+    }
+  }
+
+}

c6222ff32e67bd7a8e40a9c9917ab836228d4ee4
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-10-10 17:01:10 +0100

    Working on saving properties

--- lorix/grails-app/controllers/uk/ac/jisc/lorix/ActivityController.groovy
+++ lorix/grails-app/controllers/uk/ac/jisc/lorix/ActivityController.groovy
@@ -76,10 +76,66 @@ class ActivityController {
 
     // Look in j for property 'root' and lookup or create based on root.__oid
     result.root = lookupOrCreateReferenceProperty(j.root)
-    processThree(j.root,result.root)
+    mergeInstanceWithPostData(j.root,result.root)
+    recursiveValidate(j.root, result.root)
+    recursiveSave(j.root, result.root);
 
     result.status='OK'
     render result as JSON
+  }
+
+  def recursiveValidate(formPostData, contextObject) {
+    return true;
+  }
+
+
+  // II: This is experimental - I don't think this is the way to go, but leaving the code in place
+  // for now just to keep it around.
+  def recursiveSave(sourceContext, contextObject) {
+    log.debug("recursiveSave(...,${contextObject})");
+
+    // contextObject.getDirtyPropertyNames()
+    if ( sourceContext && contextObject ) {
+
+      def class_name_of_result_context = contextObject.class.name
+      def domain_class = grailsApplication.getDomainClass(class_name_of_result_context)
+
+      // We need to save the parent object before we create any..
+      log.debug("See if ${contextObject} (Instance of ${domain_class}  - with ID ${contextObject.id} is dirty...");
+      if ( ( contextObject.isDirty() ) || ( contextObject.id == null ) ) {
+        log.debug("yes.. save ${contextObject}");
+        if ( contextObject.save() ) {
+          log.debug("Saved ok : id is ${contextObject.id}");
+        }
+        else {
+          log.debug("Save error ${contextObject.errors}");
+        }
+      }
+      else {
+        log.debug("Nooo");
+      }
+
+      sourceContext.each { k, v ->
+        def p = domain_class.getPersistentProperty(k)  //  GrailsDomainClassProperty
+        if ( p ) {
+          if ( p.isAssociation() ) {
+            log.debug("${k} is an association for saving..");
+            if ( p.isOneToMany() ) {
+              int i=0;
+              contextObject[k].each { db_object ->
+                log.debug("Consider ${db_object}");
+                recursiveSave(v[i++],db_object)
+              }
+            }
+            else {
+            }
+          }
+        }
+      }
+    }
+
+    log.debug("recursiveSave ${contextObject} finished...");
+    return true;
   }
 
   def lookupOrCreateReferenceProperty(formPostData) {
@@ -100,17 +156,27 @@ class ActivityController {
         }
       }
     }
+
+    // Grails can get really confused if we start adding non-belongsTo objects to hasMany sets when
+    // the object is not saved. This, unfortunately, means we need to save objects before we start adding stuff
+    // to collections. Really, I would like to validate objects before saving them.
+
+    log.debug("lookupOrCreateReferenceProperty(${formPostData}) returns ${result}");
+
     result
   }
 
   // Look in sourceContext for element, and see if there is a corresponding thing in resultContext to set
   // Assume we are the root of the object tree for now.
-  def processThree(sourceContext, resultContext) {
+  def mergeInstanceWithPostData(sourceContext, resultContext) {
     if ( resultContext ) {
       def class_name_of_result_context = resultContext.class.name
       log.debug("Iterate through all properties at this level... ${class_name_of_result_context}");
       def domain_class = grailsApplication.getDomainClass(class_name_of_result_context)
 
+
+      // It might be that we should merge in ordinary properties before we "save" the object so that any
+      // not-null validation rules will pass. that sucks ass a little.
       sourceContext.each { k, v ->
         log.debug("Consider ${k} -> ${v}");
         def p = domain_class.getPersistentProperty(k)  //  GrailsDomainClassProperty
@@ -122,6 +188,13 @@ class ActivityController {
               // Synchronize the data from the form post with the data in the DB, create, update and delete records as needed.
               processOneToManyProperty(k, v, resultContext);
             }
+            else {
+              log.debug("Unhandled association type");
+            }
+          }
+          else {
+            log.debug("Setting scalar property ${k} to ${v}");
+            resultContext[k] = v
           }
         }
       }
@@ -141,6 +214,8 @@ class ActivityController {
     // Grab the collection of persistent objects under this property
     def db_coll = parentDBObject[propName]
     int num_child_objects = db_coll.size();
+
+    def methodName = 'addTo'+propName.capitalize()
              
     valueFromPost.each { child_row_data ->
       log.debug("child row: ${child_row_data}");
@@ -151,7 +226,15 @@ class ActivityController {
       else {
         log.debug("Adding a row to the collection property.. lookup or create depending on OID");
         def new_child_object = lookupOrCreateReferenceProperty(child_row_data)
-        db_coll.add(new_child_object)
+
+        if ( new_child_object ) {
+          // addTo is a convienience method that sets any reciprocal property on the associated object
+          parentDBObject."${methodName}"(new_child_object)
+          // db_coll.add(new_child_object)
+        }
+        else {
+          log.error("Something has gone amiss : adding null to a collection.. lookup or create reference property for ${child_row_data} returned null");
+        }
       }
       idx++
     }

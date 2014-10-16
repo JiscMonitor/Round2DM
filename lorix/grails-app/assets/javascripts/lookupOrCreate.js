d3a752c3c2349c4234eec74106ae1cc8197f334a
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-10-16 22:16:48 +0100

    nother step towards setting properties in nested classes

--- lorix/grails-app/assets/javascripts/lookupOrCreate.js
+++ lorix/grails-app/assets/javascripts/lookupOrCreate.js
@@ -4,13 +4,18 @@
 
   app.controller('LookupOrCreateCtrl', function($scope,$http,$log) {
 
-    $scope.init = function(baseclass, proplist) {
-      $scope.baseclass = baseclass;
-      $scope.proplist = proplist;
+    // $scope.init = function(baseclass, proplist) {
+    $scope.init = function(ctx, prop) {
+      $log.debug("lookupOrCreate::init(%o,%o)",ctx,prop);
+      $scope.ctx = ctx;
+      $scope.property = prop;
+      //$scope.baseclass = baseclass;
+      //$scope.proplist = proplist;
     }
 
     $scope.notifyRecord = function(value) {
       $log.debug("lookupOrCreate::notifyRecordFunc(%o)",value);
+      $log.debug("set property %o on %o to %o",$scope.ctx, $scope.property, value);
     }
 
     $scope.updateSearchResults = function (value) {
